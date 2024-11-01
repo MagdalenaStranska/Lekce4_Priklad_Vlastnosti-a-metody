7d002a08c332b44c52d07db5d98d2fae434be787
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-11-01 23:38:26 +0100

    Pridavam reseni

---
+++ script.js
@@ -0,0 +1,22 @@
+const title = 'Hobit - Neočekávaná cesta'
+document.body.innerHTML += title;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += title.length;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += title.toUpperCase();
+document.body.innerHTML += '<br>';
+document.body.innerHTML += title.slice(0, 5);
+document.body.innerHTML += '<br>';
+document.body.innerHTML += title.slice(title.length - 5, title.length);
+document.body.innerHTML += '<br>';
+
+
+
+// const title = "Lord of the Rings";
+// document.body.innerHTML += title.length;
+// document.body.innerHTML += `<br>`;
+// document.body.innerHTML += title.toUpperCase();
+// document.body.innerHTML += `<br>`;
+// document.body.innerHTML += title.slice(0, 5);
+// document.body.innerHTML += `<br>`;
+// document.body.innerHTML += title.slice(title.length - 5, title.length)
